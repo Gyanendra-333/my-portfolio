9fc52cb2ebe92e582b63cb3fdce68d5e3de59e97
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-05-23 10:17:21 +0530

    Hamburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Navbar.css";
 import { Link } from 'react-router-dom';
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!false);
+
     return (
         <div className='header'>
             <Link to="/">
@@ -28,7 +31,10 @@ function Navbar() {
                 </li>
             </ul>
             <div className='hamburger'>
+
+                <FaTimes size={20} style={{ color: "aliceblue" }} />
                 <FaBars size={20} style={{ color: "aliceblue" }} />
+
             </div>
 
         </div>
