f1a24cd51ab707dd30843b7d4cffcf61402d8b03
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-05-25 16:33:31 +0530

    Footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,6 +1,6 @@
 import "./Footer.css";
 import React from 'react';
-import { FaHome } from "react-icons/fa";
+import { FaFacebook, FaHome, FaLinkedin, FaMailBulk, FaPhone, FaTwitter } from "react-icons/fa";
 
 function Footer() {
     return (
@@ -8,14 +8,42 @@ function Footer() {
             <div className="footer-container">
                 <div className="left">
                     <div className="location">
-                        <FaHome />
+
+                        <FaHome size={20} style={{ color: "aliceblue", marginRight: "2rem" }} />
+                        <div>
+                            <p>Tilhar, Shahjahanpur</p>
+                            <p>Uttar Pradesh, India</p>
+                        </div>
+
+                        <div className="phone">
+                            <h4><FaPhone size={20} style={{ color: "aliceblue", marginRight: "2rem" }} /> +91-7355161229</h4>
+                        </div>
+
+
+                        <div className="mail">
+                            <h4><FaMailBulk size={20} style={{ color: "aliceblue", marginRight: "2rem" }} />pgyanendra333@gmail.com</h4>
+                        </div>
+
                     </div>
                 </div>
-                <div className="right"></div>
+                <div className="right">
+                    <h4 >About us</h4>
+                    <p>This is me Gyanendra Prakash. I am a front-End Developer. </p>
+                    <p>I have Skills HTML5, CSS3, JavaScript, Bootstarp And React Js</p>
+
+                    <div className="social">
+                        < FaFacebook size={30} style={{ color: "aliceblue", marginRight: "1rem" }} />
+
+                        < FaTwitter size={30} style={{ color: "aliceblue", marginRight: "1rem" }} />
+
+                        < FaLinkedin size={30} style={{ color: "aliceblue", marginRight: "1rem" }} />
+
+                    </div>
+                </div>
             </div>
         </div>
 
-    )
+    );
 }
 
-export default Footer
+export default Footer;
