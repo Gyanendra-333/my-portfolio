9fa082f580d6ceb19e42cad0d849064e2450f38d
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-05-25 18:53:28 +0530

    Project Hero Image

---
+++ src/components/Project_Hero_Img.js
@@ -0,0 +1,10 @@
+import "./Project_Hero_Img.css";
+import React from 'react';
+
+function Project_Hero_Img() {
+    return (
+        <div>Project_Hero_Img</div>
+    );
+}
+
+export default Project_Hero_Img;
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../footer/Footer';
+import Project_Hero_Img from '../components/Project_Hero_Img';
 
 
 function Projects() {
     return (
         <div>
             <Navbar />
+            <Project_Hero_Img />
             <Footer />
         </div>
     );
