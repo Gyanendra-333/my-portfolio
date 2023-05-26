93553b81df39818333516c532164229ba6da4957
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-05-26 17:36:33 +0530

    Work Card

---
+++ src/components/project_card/Work_Card.js
@@ -0,0 +1,10 @@
+import "./Work_Card.css";
+import React from 'react'
+
+function Work_Card() {
+    return (
+        <div>Work_Card</div>
+    )
+}
+
+export default Work_Card;
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar';
 import Footer from '../footer/Footer';
 import Project_Hero_Img from '../components/Project_Hero_Img';
 import Project_Card from '../components/project_card/Project_Card';
+import Work_Card from '../components/project_card/Work_Card';
 
 
 function Projects() {
@@ -10,6 +11,7 @@ function Projects() {
         <div>
             <Navbar />
             <Project_Hero_Img heading="PROJECTS." text="There Are Some Projects of me." />
+            <Work_Card />
             <Project_Card />
             <Footer />
         </div>
